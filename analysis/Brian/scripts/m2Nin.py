eee6244e0e02a11b27581fc7444291b639ee5629
Brian Su <brisu@student.ubc.ca>
2020-11-12 11:53:41 +0800

    m2Nin WIP

--- analysis/Brian/scripts/m2Nin.py
+++ analysis/Brian/scripts/m2Nin.py
@@ -1,9 +1,8 @@
-def load_and_process(filePath):
+def load_and_process_Nintendo(filePath):
     import pandas as pd
-
     dfClean = (
         pd.read_csv(filePath,
-        usecols = ["Rank", "Name", "Genre", "Global_Sales", "Year", "Platform"]) # So far our only focus of work
+        usecols = ["Rank", "Name", "Publisher", "Platform", "Genre", "Global_Sales", "Year"]) # So far our only focus of work
         .sort_values(by= "Global_Sales", ascending = False) # Cash money check
         .dropna(axis = 0, thresh = 3) # Drop any NA values completely
         .reset_index(drop=True) # Reset index so we don't mess up orders
@@ -11,7 +10,11 @@ def load_and_process(filePath):
 
     dfWii = (
         dfClean
-        .loc[lambda x: x["Platform"] == "Wii"] # We love Wii only wanna play Wii
+        .loc[(dfClean["Publisher"] == "Nintendo")]
+        #.loc[lambda x: x["Publisher"] == "Nintendo"] # We love Wii only wanna play Wii
+        #.loc[lambda x: x["Platform"] == "Wii" | x["Platform"] == "DS"]
+        #.loc[lambda x: x["Platform"] == "DS"]
+        .loc[(dfClean["Platform"] == "Wii") | (dfClean["Platform"] == "DS")]
         .loc[lambda x: x["Year"] >= 2000] # Check for only recent games
         .loc[lambda x: x["Rank"] <= 2500] # Nothing ranked lower tan 2500
         .assign(Averaged_Sales = lambda x: x["Global_Sales"]/4) # Average out sales
@@ -20,4 +23,4 @@ def load_and_process(filePath):
     )
     return dfWii
 
-load_and_process("../../data/raw/Video_game_sales_db.csv")
+load_and_process_Nintendo("../../data/raw/Video_game_sales_db.csv")
